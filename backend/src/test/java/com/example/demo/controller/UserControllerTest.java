fb4a2080740ecc29dec53d46ce6463b8e30d6be7
Pasindurandima <167874999+Pasindurandima@users.noreply.github.com>
2026-09-17 11:04:41 +0530

    remove h2

--- backend/src/test/java/com/example/demo/controller/UserControllerTest.java
+++ backend/src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -14,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
-@ActiveProfiles("h2")
 class UserControllerTest {
 
     @Autowired
